140fb541d7e8306a3e91b5165d58d5bc3c32ffe6
ahyaemon <iamahyaemon@gmail.com>
2022-08-27 14:22:08 +0900

    update reverse

--- reverse/reverseCipher.test.ts
+++ reverse/reverseCipher.test.ts
@@ -1,7 +1,49 @@
 import { assertEquals } from "https://deno.land/std@0.65.0/testing/asserts.ts";
-import {reverse} from "./reverseCipher.ts";
+import {reverse, reverse2, reverse3, reverse4} from "./reverseCipher.ts";
 
+const plainText = 'Three can keep a secret, if two of them are dead.'
+const expected = '.daed era meht fo owt fi ,terces a peek nac eerhT'
 
-Deno.test("test", () => {
-    assertEquals(reverse('hello'), 'olleh')
+Deno.test("reverse", () => {
+    assertEquals(reverse(plainText), expected)
+    // measureSeconds(() => { reverse(plainText) }, 1000)
 })
+
+Deno.test("reverse2", () => {
+    assertEquals(reverse2(plainText), expected)
+    // measureSeconds(() => { reverse2(plainText) }, 1000)
+})
+
+Deno.test("reverse3", () => {
+    assertEquals(reverse3(plainText), expected)
+    // measureSeconds(() => { reverse3(plainText) }, 1000)
+})
+
+Deno.test("reverse4", () => {
+    assertEquals(reverse4(plainText), expected)
+    // measureSeconds(() => { reverse4(plainText) }, 1000)
+})
+
+Deno.test({
+    name: 'measure',
+    fn: () => {
+        const result1 = measureSeconds(() => { reverse(plainText) }, 1000)
+        console.log({result1})
+        const result2 = measureSeconds(() => { reverse2(plainText) }, 1000)
+        console.log({result2})
+        const result3 = measureSeconds(() => { reverse3(plainText) }, 1000)
+        console.log({result3})
+        const result4 = measureSeconds(() => { reverse4(plainText) }, 1000)
+        console.log({result4})
+    },
+    ignore: false,
+})
+
+function measureSeconds(f: () => void, times: number): number {
+    const t0 = performance.now()
+    for(let i = 0; i < times; i++) {
+        f()
+    }
+    const t1 = performance.now()
+    return t1 - t0
+}
--- reverse/reverseCipher.ts
+++ reverse/reverseCipher.ts
@@ -1,3 +1,24 @@
 export function reverse(plainText: string): string {
     return plainText.split('').reverse().join('')
 }
+
+export function reverse2(plainText: string): string {
+    let encrypted = ''
+    for(let s of plainText) {
+        encrypted = s + encrypted
+    }
+
+    return encrypted
+}
+
+export function reverse3(plainText: string): string {
+    return plainText.split('').reduce((acc, cur) => cur + acc)
+}
+
+export function reverse4(plainText: string): string {
+    let encrypted = ''
+    for (let i = 0; i < plainText.length; i++) {
+        encrypted = plainText[i] + encrypted
+    }
+    return encrypted
+}
